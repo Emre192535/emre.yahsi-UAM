92fa121b5f31ca5e40369a64d52b9c5ca94e330e
emre yahsi <emre.yahssi@gmail.com>
2022-11-21 17:10:19 +0100

    new project

---
+++ ilcsp_05.py
@@ -0,0 +1,15 @@
+'''
+Exercise 05
+Write a program that will calculate circle area (and print to the terminal), 
+radius is 5 cm, 
+lets's assume pi = 3.14
+A=𝜋𝑟^2 (this is our formula)
+expected result: e.g., 
+
+"Circle area is: 78.5 cm2"
+
+'''
+
+rad = int(input("please enter the raius of the circle=  "))
+area = 3.14 * rad*rad
+print("circle area is",  area)
