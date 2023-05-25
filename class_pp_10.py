6c9e0d1e3c93c2ef30539a6b20d6b946f4c9a932
Emre192535 <82039402+Emre192535@users.noreply.github.com>
2023-05-25 16:43:37 +0200

    Add files via upload

---
+++ class_pp_10.py
@@ -0,0 +1,33 @@
+import re
+import sys
+# if re.search('^a[b-e]', sys.argv[1]):
+#     print("its a match")
+
+# else:
+#     print("no match")
+
+
+# if re.search('^[b-e]a$', sys.argv[1]):
+#     print("its a match")
+
+# else:
+#     print("no match")
+
+# if re.search('[a-z]a$', sys.argv[[1]):
+#     print("a match")
+
+# else:
+#     print("no match")
+
+# if re.search('[a-s]ana$', sys.argv[1]):
+#     print("a match")
+
+# else:
+#     print("no match")
+
+
+if re.search('he*o', sys.argv[1]):
+    print("a match")
+
+else:
+    print("no match")
